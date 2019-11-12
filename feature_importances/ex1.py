7064092613a42ea9227de030ff3ab22b8bfb9207
Kyle Nickerson <kyle.nickerson8@gmail.com>
2019-11-12 12:48:31 -0330

    feat impor example

---
+++ feature_importances/ex1.py
@@ -0,0 +1,23 @@
+
+
+from sklearn.datasets import load_breast_cancer
+import pandas as pd
+from sklearn.preprocessing import scale
+
+data = load_breast_cancer()
+type(data)
+
+df = pd.DataFrame(data.data)
+df.columns = data.feature_names
+df
+
+
+
+df = pd.DataFrame(scale(data.data))
+df.columns = data.feature_names
+df
+
+
+from plotly import graph_objects as go
+
+go.Figure(data=go.Heatmap(z=df.values)).show()
